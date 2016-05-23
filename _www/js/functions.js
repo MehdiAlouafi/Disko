dbc12e9f35dbbcc7f7a4bdae957e111403e25d2e
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-05-23 06:52:27 +0200

    Manque plus que le konami code

--- _www/js/functions.js
+++ _www/js/functions.js
@@ -1,30 +1,32 @@
-// // check to make sure that the browser can handle window.addEventListener
-// if (window.addEventListener) {
-//     // create the keys and konami variables
-//     var keys = [],
-//         konami = "38,38,40,40,37,39,37,39,66,65";
-//     // bind the keydown event to the Konami function
-//     window.addEventListener("keydown", function(e){
-//         // push the keycode to the 'keys' array
-//         keys.push(e.keyCode);
-//         console.log(keys);
-//         //Je rajoute cette condition afin de pouvoir réinitialiser le tableau si les touches cliquées dépassent
-//         // la longueur de konami
-//         if(keys.length > konami.split(',').length){
-//           keys = [];
-//         }
-//         // and check to see if the user has entered the Konami code
-//         if (keys.toString() === konami) {
-//             // do something such as:
-//             alert('salut!')
-//               // console.log("bonjour");
-//             // and finally clean up the keys array
-//             keys = [];
-//         };
-//     }, true);
-// };
 'use strict',
 $(function(){
+  // check to make sure that the browser can handle window.addEventListener
+  var tl = new TimelineMax(),
+      posX = $(window).height() / 2,
+      posY = $(window).width() / 2;
+  if (window.addEventListener) {
+    // create the keys and konami variables
+    var keys = [],
+    konami = "38,38,40,40,37,39,37,39,66,65";
+    // bind the keydown event to the Konami function
+    window.addEventListener("keydown", function(e){
+      // push the keycode to the 'keys' array
+      keys.push(e.keyCode);
+      console.log(keys);
+      //Je rajoute cette condition afin de pouvoir réinitialiser le tableau si les touches cliquées dépassent
+      // la longueur de konami
+      if(keys.length > konami.split(',').length){
+        keys = [];
+      }
+      // and check to see if the user has entered the Konami code
+      if (keys.toString() === konami) {
+        // do something such as:
+        tl.to($('section,footer'),0.3,{opacity:0})
+        tl.to($('.logo'), 1 , {x: posX - 30 , y: posY - 30})
+        keys = [];
+      };
+    }, true);
+  };
   var sliders  = $('.slider__slide'),
       previous = $('.slider__control--previous'),
       next     = $('.slider__control--next');
